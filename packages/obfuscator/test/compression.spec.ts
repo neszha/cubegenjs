93c3cac9433fcbacdb55ce708ddda526c6a7f62b
Fanesa Hadi Pramana <fanesahadi@gmail.com>
2024-10-08 09:21:29 +0700

    feat(obfuscator): add aes to test compresion ratio

--- packages/obfuscator/test/compression.spec.ts
+++ packages/obfuscator/test/compression.spec.ts
@@ -3,38 +3,52 @@ import fs from 'fs-extra'
 import { CubegenObfuscator } from '../src/index'
 
 const MODULE_PATH_DIR = path.resolve(__dirname, '../')
+const sampleTestAesPath = path.join(MODULE_PATH_DIR, 'test/examples/aes.sample-test.js')
 const sampleTestMd5Path = path.join(MODULE_PATH_DIR, 'test/examples/md5.sample-test.js')
 const sampleTestHelloWordPath = path.join(MODULE_PATH_DIR, 'test/examples/hello-word.sample-test.js')
 
 describe('Test Compression Bundler Module', () => {
     it('Calculate compression ratio.', async () => {
-        const obfuscator1 = new CubegenObfuscator(sampleTestMd5Path)
-        const obfuscator2 = new CubegenObfuscator(sampleTestHelloWordPath)
+        const obfuscator1 = new CubegenObfuscator(sampleTestHelloWordPath)
+        const obfuscator2 = new CubegenObfuscator(sampleTestAesPath)
+        const obfuscator3 = new CubegenObfuscator(sampleTestAesPath)
         const result1 = obfuscator1.transform()
         const result2 = obfuscator2.transform()
+        const result3 = obfuscator3.transform()
 
         const compressionData: any[] = []
 
         // Calculate file size result 1.
-        const sourceFileSize1 = fs.statSync(sampleTestMd5Path).size
+        const sourceFileSize1 = fs.statSync(sampleTestHelloWordPath).size
         const outputFileSize1 = fs.statSync(result1.outputTempPath).size
         const compressionRatio1 = outputFileSize1 / sourceFileSize1
         compressionData.push({
-            Entry: 'md5.sample-test.js',
+            Entry: 'hello-word.sample-test.js',
             'Source Size (Bytes)': sourceFileSize1,
             'Output Size (Bytes)': outputFileSize1,
             'Compression Ratio': compressionRatio1
         })
 
         // Calculate file size result 2.
-        const sourceFileSize2 = fs.statSync(sampleTestHelloWordPath).size
+        const sourceFileSize2 = fs.statSync(sampleTestAesPath).size
         const outputFileSize2 = fs.statSync(result2.outputTempPath).size
         const compressionRatio2 = outputFileSize2 / sourceFileSize2
         compressionData.push({
-            Entry: 'hello-word.sample-test.js',
+            Entry: 'aes.sample-test.js',
             'Source Size (Bytes)': sourceFileSize2,
             'Output Size (Bytes)': outputFileSize2,
             'Compression Ratio': compressionRatio2
+        })
+
+        // Calculate file size result 3.
+        const sourceFileSize3 = fs.statSync(sampleTestMd5Path).size
+        const outputFileSize3 = fs.statSync(result3.outputTempPath).size
+        const compressionRatio3 = outputFileSize3 / sourceFileSize3
+        compressionData.push({
+            Entry: 'md5.sample-test.js',
+            'Source Size (Bytes)': sourceFileSize3,
+            'Output Size (Bytes)': outputFileSize3,
+            'Compression Ratio': compressionRatio3
         })
 
         // Show data.
