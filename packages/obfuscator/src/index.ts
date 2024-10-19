6507dad9cb318b7a9b8dd744e7563460058e817e
Fanesa Hadi Pramana <fanesahadi@gmail.com>
2024-10-20 00:47:07 +0700

    feat: publish obfuscator version 0.1.1 to npm

--- packages/obfuscator/src/index.ts
+++ packages/obfuscator/src/index.ts
@@ -2,9 +2,9 @@ import path from 'path'
 import fs from 'fs-extra'
 import { createHash } from 'crypto'
 import JavaScriptObfuscator from 'javascript-obfuscator'
-import { type FilePath } from './interfaces/Common'
-import obfuscatorDefaultConfig from './configs/obfuscator.default-config'
 import { type ObfuscatorTargetEnvironment, type CubegenObfuscatorOptions, type CubegenObfuscatorResponse } from './interfaces/Obfuscator'
+import obfuscatorDefaultConfig from './configs/obfuscator.default-config'
+import { type FilePath } from './interfaces/Common'
 
 const MODULE_PATH_DIR = path.resolve(__dirname, '../')
 const MODULE_OBFUSCATOR_CACHE_PATH_DIR = path.resolve(MODULE_PATH_DIR, '.obfuscator-cache')
